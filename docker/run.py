83a1df39e5ae6fd961d523474d2f235afd379edd
Keenan Graham <ksgraham@stanford.edu>
2024-04-22 17:39:38 -0700

    Update

--- docker/run.py
+++ docker/run.py
@@ -1,8 +1,10 @@
+import base64
 import json
 import os
 
 
 if __name__ == '__main__':
     print('helllo')
-    print(os.environ.get('SA_SECRET', 'no secret'))
-    print(json.loads(os.environ.get('SA_SECRET', '{}')))
+    secret = os.environ.get('SA_SECRET', '{}')
+    print(secret)
+    print(json.loads(base64.b64decode(secret).decode('utf-8')))
